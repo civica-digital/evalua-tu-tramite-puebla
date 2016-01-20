6e518b925ed0cc410f8ecffa1312e54d0dc244d0
mikesaurio <mikesaurio@gmail.com>
2016-01-20 14:27:21 -0600

    se pueden seleccionar 2 service admins para los servicios

--- app/assets/javascripts/public/loadChosen.js
+++ app/assets/javascripts/public/loadChosen.js
@@ -6,3 +6,14 @@ var loadChosen = function(){
        width: '100%'
     });
 };
+
+var loadChosenlimit = function(){
+    $('.chosen-select-limit').chosen({
+       allow_single_deselect: true,
+       no_results_text: 'No hay resultados',
+       placeholder_text: 'Selecciona una opción',
+       width: '100%',
+       max_selected_options: 1
+    });
+};
+
